fef04ff02064658be9e53210345778a2f1ce8741
nareshbabumandula <naresh223@gmail.com>
2024-01-03 08:43:49 +0530

    03-Jan-2024: Selenium Locators

--- SeleniumJavaProject/src/com/selenium/scripts/LocatorsTest.java
+++ SeleniumJavaProject/src/com/selenium/scripts/LocatorsTest.java
@@ -13,9 +13,30 @@ public class LocatorsTest {
 		driver = new ChromeDriver();
 		driver.get("https://www.mycontactform.com");
 		driver.manage().window().maximize();
+		// 1. ID Locator
 		driver.findElement(By.id("user")).sendKeys("Prabhakar");
+		// 2. Name Locator
 		driver.findElement(By.name("pass")).sendKeys("Secure*1234");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
+		// 3. Classname Locator
+		driver.findElement(By.className("btn_log")).click();
+		Thread.sleep(2000);
+		// 4. Linktext Locator
+		driver.findElement(By.linkText("Sample Forms")).click();
+		Thread.sleep(2000);
+		// 5. Partial Linktext
+		driver.findElement(By.partialLinkText("Featu")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Sample Forms")).click();
+		// 6. CSS Selector
+		driver.findElement(By.cssSelector("input[name='email_to[]']")).click();
+		Thread.sleep(2000);
+		// 7. Tag name Locator
+		driver.findElement(By.tagName("input")).sendKeys("Ravi");
+		Thread.sleep(2000);
+		// 8. XPath Locator
+		driver.findElement(By.xpath("//input[@id='subject']")).sendKeys("ravikanth@gmail.com");
+		Thread.sleep(2000);
 		driver.quit();
 	}
 
