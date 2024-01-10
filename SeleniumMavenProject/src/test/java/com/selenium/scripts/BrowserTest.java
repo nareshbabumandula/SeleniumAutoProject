b2dc47a3fb95459e65c8395ab6b4d198f06ff64b
nareshbabumandula <naresh223@gmail.com>
2024-01-10 08:15:24 +0530

    Added Maven Project for Selenium

---
+++ SeleniumMavenProject/src/test/java/com/selenium/scripts/BrowserTest.java
@@ -0,0 +1,40 @@
+package com.selenium.scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserTest {
+
+	static WebDriver driver;
+
+	public static void browserTest(String strBrowser) throws InterruptedException {
+		switch (strBrowser) {
+		case "chrome": case "google":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver(); //A WebDriver implementation that controls a Chrome browser running on the local machine.
+			break;
+		case "edge": case "chromium":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			break;
+		default:
+			System.out.println("Invalid browser..!");
+			break;
+		}
+		driver.get("https://www.mycontactform.com/");
+		driver.manage().window().maximize();
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		browserTest("chrome");
+		browserTest("edge");
+	}
+
+}
