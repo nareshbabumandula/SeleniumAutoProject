e8e1617111ec86d2243f4f92ca1ae9a7ae61568a
nareshbabumandula <naresh223@gmail.com>
2024-02-01 08:50:54 +0530

    01-Feb-2024: Naresh: TestNG Annotations

---
+++ SeleniumMavenProject/src/test/java/com/testng/annotations/TestNGAnnotations.java
@@ -0,0 +1,91 @@
+package com.testng.annotations;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestNGAnnotations extends Generic {
+	
+	//The @BeforeTest annotated method will be executed before the execution of all the test methods of available classes belonging to that folder.
+	@BeforeTest(groups = "smoke")
+	void accessSite() {
+		System.out.println("Accessed Amazon site..!");
+	}
+
+	// The @BeforeClass annotated method will be executed before the first method of the current class is invoked.
+	@BeforeClass(groups = "smoke")
+	void login() {
+		System.out.println("Logged into the Amazon site..!");
+	}
+
+	@Parameters("product")
+	@Test(priority = 1, groups = "smoke")
+	void searchProduct(String product) {
+		System.out.println("Searched the product : " + product);
+	}
+
+	@Test(priority = 2, groups = "smoke")
+	void addToCart() {
+		System.out.println("Added the product to the cart..!");
+	}
+	
+	@Test(dependsOnMethods = {"addToCart"})
+	void checkOut() {
+		System.out.println("Checkedout the product..!");
+	}
+
+	@Test(dependsOnMethods = {"checkOut"})
+	void placeOrder() {
+		System.out.println("Order placed successfully..!");
+	}
+	
+	// The @AfterClass annotated method will be invoked after the execution of all the test methods of the current class.
+	@AfterClass
+	void viewReceipt() {
+		System.out.println("Viewed the receipt..!");
+	}
+	
+	// The @AfterTest annotated method will be executed after the execution of all the test methods of available classes belonging to that folder.
+	@AfterTest(groups = "smoke")
+	void logout() {
+		System.out.println("Logged out from Amazon site..!");
+	}
+	
+	// The @BeforeMethod annotated method will be executed before each test method will run.
+	@BeforeMethod
+	void verifyProduct() {
+		System.out.println("Verified the product..!");
+	}
+	
+	// The @AfterMethod annotated method will run after the execution of each test method.
+	@AfterMethod
+	void verifyQuanity() {
+		System.out.println("Verified the quantity..!");
+	}
+
+	
+}
+
+class Generic{
+	
+	// The @BeforeSuite annotated method will run before the execution of all the test methods in the suite.
+	@BeforeSuite(groups = "smoke")
+	void launchBrowser() {
+		System.out.println("Launched the browser..!");
+	}
+	
+	// The @AfterSuite annotated method will run after the execution of all the test methods in the suite.
+	@AfterSuite(groups = "smoke")
+	void closeBrowser() {
+		System.out.println("Closed the browser..!");
+	}
+
+	
+}
